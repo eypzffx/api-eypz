699caed72d00c627c81af049957da936ad83c42f
Iron Man <narendrannirmal6753@gmail.com>
2024-06-18 01:39:17 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,17 @@
+from flask import Flask
 
+
+
+app = Flask(__name__)
+
+#home
+@app.route('/')
+def home():
+    return 'Api is running somewhere!'
+
+
+
+
+#dont touch this thing
+if __name__ == '__main__':
+    app.run(port=3000)
