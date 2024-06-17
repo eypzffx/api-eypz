6a5757d45b81a0263d875ad6dfa7a3dc6903e8fe
Iron Man <narendrannirmal6753@gmail.com>
2024-06-18 02:03:21 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,24 @@
-from flask import Flask
-
-
+from flask import Flask, Response
+import random
+import requests
+import json
 
 app = Flask(__name__)
+
+with open('../video.json', 'r') as file:
+    urls = json.load(file)
 
 #home
 @app.route('/')
 def home():
     return 'Api is running somewhere!'
-
+ 
+#random videos    
+@app.route('/videos', methods=['GET'])
+def video():
+    selected_url = random.choice(urls)
+    response = requests.get(selected_url, stream=True)
+    return Response(response.iter_content(chunk_size=1024), content_type=response.headers['content-type'])
 
 
 
