2e418e9113aa00c61a9e28d6dd00b646e409f590
Iron Man <narendrannirmal6753@gmail.com>
2024-06-20 15:13:11 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,18 +5,20 @@ import json
 
 app = Flask(__name__)
 
-with open('video.json', 'r') as f:
-    video_urls = json.load(f)
+IronMan = [
+    "https://cdn.discordapp.com/attachments/947033103988314143/1252355487706648710/VID-20240608-WA0044.mp4?ex=6671ea6f&is=667098ef&hm=a702c6325ed8e2df64956861c33495ca1ba90340e946735ecb91c2b29c705384&",
+    "https://cdn.discordapp.com/attachments/947033103988314143/1252360857946689717/20240418152629633.mp4?ex=6671ef70&is=66709df0&hm=2ec880e4f4c18631dcf189c273c4c928173eaf8a9c9f96ca7c01b30955cab9bf&"
+]
 
-#home
+# Home 
 @app.route('/')
 def home():
     return 'API is running somewhere!'
 
-#vidoe 
+#vidoe
 @app.route('/video', methods=['GET'])
 def anime():
-    video_url = random.choice(video_urls)
+    video_url = random.choice(IronMan)
     
     def generate():
         with requests.get(video_url, stream=True) as r:
