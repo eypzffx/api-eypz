eb049fc69dbe546792b6a7929b0c2790bf0902a0
akshay-god01 <akshayeypz0@gmail.com>
2024-07-08 00:53:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import random
 app = Flask(__name__)
 
 # Define the directory where your video files are stored
-VIDEO_DIRECTORY = os.path.join(os.getcwd(), 'video')  # Assuming 'video' is your directory name
+VIDEO_DIRECTORY = os.path.join(os.getcwd(), 'videos')
 
-# Get a list of all video files in the VIDEO_DIRECTORY
+# List all video files in the VIDEO_DIRECTORY
 video_files = os.listdir(VIDEO_DIRECTORY)
 
 @app.route('/anime', methods=['GET'])
 def serve_random_video():
     try:
-        # Select a random video file from the list
-        anime = random.choice(video_files)
+        # Choose a random video file from the list
+        random_video = random.choice(video_files)
         return send_from_directory(VIDEO_DIRECTORY, random_video)
     except IndexError:
         return "No videos found", 404
