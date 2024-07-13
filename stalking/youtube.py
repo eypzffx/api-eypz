ba7e6344a74743a97f54f870d88404adf68841be
akshay-god01 <akshayeypz0@gmail.com>
2024-07-13 13:11:08 +0530

    Update youtube.py

--- stalking/youtube.py
+++ stalking/youtube.py
@@ -1,22 +1,21 @@
 import requests
 import re
-from flask import jsonify
 
 def download_thumbnail(video_url):
     video_id = extract_video_id(video_url)
     if not video_id:
-        return {'error': 'Invalid YouTube URL'}, 400, {}
+        return None, 'Invalid YouTube URL'
 
     thumbnail_url = f'https://img.youtube.com/vi/{video_id}/maxresdefault.jpg'
 
     try:
         response = requests.get(thumbnail_url)
         if response.status_code == 200:
-            return response.content, 200, {'Content-Type': 'image/jpeg'}
+            return thumbnail_url, None
         else:
-            return {'error': 'Failed to fetch thumbnail'}, 500, {}
+            return None, 'Failed to fetch thumbnail'
     except Exception as e:
-        return {'error': str(e)}, 500, {}
+        return None, str(e)
 
 def extract_video_id(url):
     regex = r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})"
