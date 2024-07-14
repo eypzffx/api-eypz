15cd6ef582116649eab1388e2ceac9dd89cf518e
akshay-god01 <akshayeypz0@gmail.com>
2024-07-14 14:08:54 +0530

    Create instagram.py

---
+++ app/instagram.py
@@ -0,0 +1,17 @@
+# app/instagram.py
+from flask import Blueprint, jsonify, request
+from stalking.insta import get_instagram_profile
+
+instagram_bp = Blueprint('instagram', __name__)
+
+@instagram_bp.route('/insta', methods=['GET'])
+def get_insta_profile():
+    username = request.args.get('username')
+    if not username:
+        return jsonify({'error': 'Missing username parameter'}), 400
+
+    profile_info, error = get_instagram_profile(username)
+    if error:
+        return jsonify({'error': error}), 500
+
+    return jsonify(profile_info)
