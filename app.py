4ad14cb677b99f18676d0236875b559f7376e884
akshay-god01 <akshayeypz0@gmail.com>
2024-07-16 09:28:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask, render_template
 from app.lyrics import lyrics_bp
 from app.media import media_bp
@@ -10,8 +8,7 @@ from app.crypto import crypto_bp
 from app.search import search_bp
 from app.nsfw import nsfw_bp
 from app.details import details_bp
-from app.trivia import trivia_bp
-from app.pin import pin_bp  # Import pin_bp from app/pin.py
+from app.trivia import trivia_bp  # Import trivia blueprint
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
@@ -28,13 +25,10 @@ app.register_blueprint(search_bp)
 app.register_blueprint(nsfw_bp)
 app.register_blueprint(details_bp)
 app.register_blueprint(trivia_bp, url_prefix='/trivia')  # Register trivia blueprint with URL prefix
-app.register_blueprint(pin_bp, url_prefix='/pin')  # Register pin blueprint with URL prefix
 
-# Route to serve the main index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Run the app if executed directly
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
