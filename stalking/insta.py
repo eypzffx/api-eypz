0e041770bbd8621cf5d0a062c0f4f505255df59d
Akshay <akshayeypz0@gmail.com>
2024-08-22 02:37:43 +0530

    Update insta.py

--- stalking/insta.py
+++ stalking/insta.py
@@ -41,7 +41,7 @@ def download_instagram_media(url):
 
     return media_urls
 
-@media_downloader_bp.route('/download', methods=['GET'])
+@media_downloader_bp.route('/insta', methods=['GET'])
 def download_media():
     url = request.args.get('url')
     if not url:
