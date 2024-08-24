f9499d189a8824ac87f5245e0a2c4ecaa013f4e7
Akshay <akshayeypz0@gmail.com>
2024-08-24 23:43:08 +0530

    Create aio.py

---
+++ app/aio.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, jsonify
+import requests
+import os
+
+aio_bp = Blueprint('aio', __name__)
+
+@aio_bp.route('/aio', methods=['GET'])
+def aio():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({'error': 'URL parameter is required'}), 400
+
+    # Load API key from environment variables
+    api_key = os.getenv('API_KEY')
+
+    # Build the request URL
+    request_url = f'https://api.betabotz.eu.org/api/download/allin?url={url}&apikey={api_key}'
+
+    try:
+        # Send the request to the external server
+        response = requests.get(request_url)
+        response.raise_for_status()  # Raise an error for bad responses
+
+        # Get the JSON response from the external server
+        data = response.json()
+
+        # Modify the JSON response
+        if 'creator' in data:
+            data['creator'] = 'Eypz'
+
+        # Return the modified JSON response
+        return jsonify(data)
+    except requests.RequestException as e:
+        return jsonify({'error': str(e)}), 500
