a33cd96709649b7462d9720596a032e5de44a7a2
Akshay <akshayeypz0@gmail.com>
2024-08-24 19:41:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+# app.py
 from flask import Flask, render_template
 from flask_cors import CORS
 
 # Import all existing blueprints
 from app.lyrics import lyrics_bp
 from app.media import media_bp
-from app.youtube import youtube_bp  # Import the YouTube blueprint
-from app.insta import insta_bp  # Import the Instagram blueprint
+from app.youtube import youtube_bp  # Import the Instagram blueprint
+from app.insta import insta_bp  # Import the Insta blueprint
 from app.weather import weather_bp
 from app.crypto import crypto_bp
 from app.search import search_bp
@@ -24,8 +25,8 @@ CORS(app)  # Enable CORS for all routes
 # Register all existing blueprints
 app.register_blueprint(lyrics_bp)
 app.register_blueprint(media_bp)
-app.register_blueprint(youtube_bp)  # Register the YouTube blueprint
-app.register_blueprint(insta_bp)  # Register the Instagram blueprint
+app.register_blueprint(youtube_bp)  # Register the Instagram blueprint
+app.register_blueprint(insta_bp)  # Register the Insta blueprint
 app.register_blueprint(weather_bp)
 app.register_blueprint(crypto_bp)
 app.register_blueprint(search_bp)
@@ -37,7 +38,6 @@ app.register_blueprint(image_search_bp)
 app.register_blueprint(spotify_bp)
 app.register_blueprint(spotifys_bp)
 app.register_blueprint(pin_bp)
-
 # Route for index page
 @app.route('/')
 def index():
