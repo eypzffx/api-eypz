892c9da78793a73d2c2b04180ad0fdd9c04de6f0
Akshay <akshayeypz0@gmail.com>
2024-08-27 21:09:29 +0530

    Update search.py

--- app/search.py
+++ app/search.py
@@ -1,21 +1,37 @@
-# app/search.py
-from flask import Blueprint, jsonify, request
-from info.youtube import search_youtube_videos
+from flask import Blueprint, request, jsonify
+from youtubesearchpython import VideosSearch
 
 search_bp = Blueprint('search', __name__)
 
-@search_bp.route('/youtube', methods=['GET'])
-def search_youtube():
-    query = request.args.get('search')
+def format_duration(seconds):
+    mins, secs = divmod(seconds, 60)
+    return f"{mins}:{secs:02d}"
 
+def search_youtube(query):
+    search = VideosSearch(query, limit=20)
+    result = search.result()
+    videos = result['result']
+    return [
+        {
+            "title": video['title'],
+            "author": video['channel']['name'],
+            "duration": format_duration(int(video['duration'].split(':')[0]) * 60 + int(video['duration'].split(':')[1])),
+            "url": video['link']
+        }
+        for video in videos
+    ]
+
+@search_bp.route('/ytdl/search', methods=['GET'])
+def search_videos():
+    query = request.args.get('query')
     if not query:
-        return jsonify({'error': 'Missing search query parameter `search`'}), 400
+        return jsonify({"error": "No query provided"}), 400
 
-    try:
-        results, status_code = search_youtube_videos(query)
-        if status_code == 200:
-            return jsonify(results)
-        else:
-            return jsonify(results), status_code
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    video_list = search_youtube(query)
+    if not video_list:
+        return jsonify({"error": "No videos found"}), 404
+
+    return jsonify({
+        "Creator": "Eypz",
+        "results": video_list
+    })
