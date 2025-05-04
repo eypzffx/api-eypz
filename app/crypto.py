bb9d004637624e0e05105557f4d194e903e77ec5
eypzffx <eypzbrazil@gmail.com>
2025-05-04 19:56:53 +0530

    Delete app directory

--- app/crypto.py
+++
@@ -1,9 +0,0 @@
-# app/crypto.py
-from flask import Blueprint
-from info.crypto import get_crypto
-
-crypto_bp = Blueprint('crypto', __name__)
-
-@crypto_bp.route('/info/crypto', methods=['GET'])
-def info_crypto():
-    return get_crypto()
--- app/info.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-info_bp = Blueprint('info', __name__)
-
-@info_bp.route('/info', methods=['GET'])
-def get_info():
-    return jsonify({"info": "This is your Flask API"})
